2041c31909f9891087013e88c509fa12bd4141ed
Stasyaan <72401216+Stasyaan@users.noreply.github.com>
2021-04-19 21:15:23 +0300

    Delete public_html directory

--- public_html/js/share.js
+++
@@ -1,7 +0,0 @@
-
-
-
-<!-- Put this script tag to the place, where the Share button will be -->
-function share_VK(){
-	document.write(VK.Share.button({url: "https://bsuir.lab.bhuser.ru/mobile.php"},{type: "round", text: "Поделиться"}));
-}
